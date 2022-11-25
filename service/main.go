cef4783906ae5abcf9a8eca8827d55f4b263a63f
Chris F Ravenscroft <chris@voilaweb.com>
2022-11-25 14:10:23 -0800

    Self-install service

--- service/main.go
+++ service/main.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/kardianos/service"
 	"github.com/rs/zerolog"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
@@ -20,6 +22,22 @@ var (
 	log       zerolog.Logger
 	srv       *http.Server
 	procGroup *sync.WaitGroup
+	plist     = `<?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+        <dict>
+            <key>Label</key>
+            <string>com.voilaweb.fusion.CraftyThings</string>
+            <key>ProgramArguments</key>
+            <array>
+              <string>/usr/local/bin/craftythingshelper</string>
+            </array>
+            <key>RunAtLoad</key>
+            <true/>
+            <key>StandardErrorPath</key>
+            <string>/tmp/craftythings.err</string>
+        </dict>
+    </plist>`
 )
 
 type addquery struct {
@@ -29,9 +47,21 @@ type addquery struct {
 
 type program struct{}
 
+func runCmd(name string, arg ...string) *bytes.Buffer {
+	buf := new(bytes.Buffer)
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = buf
+	cmd.Stderr = buf
+	if err := cmd.Run(); err != nil {
+		log.Error().Err(err).Msg("Exec error")
+		return nil
+	}
+	return buf
+}
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	headerContentTtype := r.Header.Get("Content-Type")
-	if headerContentTtype != "application/json" {
+	headerContentType := r.Header.Get("Content-Type")
+	if headerContentType != "application/json" {
 		log.Error().Msg("Error. Form is not json-encoded.")
 		return
 	}
@@ -45,15 +75,12 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	log.Debug().Str("title", q.Title).Msg("Debug")
 	log.Debug().Str("content", q.Content).Msg("Debug")
 
-	buf := new(bytes.Buffer)
-	cmd := exec.Command("/usr/bin/osascript",
+	buf := runCmd("/usr/bin/osascript",
 		"-e", "tell application \"Things3\"",
 		"-e", "set newToDo to make new to do with properties {name:\""+strings.ReplaceAll(q.Title, `"`, `\"`)+"\", notes:\""+strings.ReplaceAll(q.Content, `"`, `\"`)+"\"}",
 		"-e", "end tell")
-	cmd.Stdout = buf
-	cmd.Stderr = buf
-	if err := cmd.Run(); err != nil {
-		log.Error().Err(err).Msg("Exec error")
+	if buf == nil {
+		return
 	}
 	gp := re.FindAllStringSubmatch(buf.String(), -1)
 	if len(gp) > 0 {
@@ -98,10 +125,65 @@ func (p *program) Stop(s service.Service) error {
 	return nil
 }
 
+func selfInstall() (exit bool) {
+	buf := runCmd("/bin/sh", "-c", "/usr/bin/pgrep craftythingshelper || true")
+	if buf == nil {
+		return true
+	}
+	if len(buf.Bytes()) > 0 {
+		log.Error().Msg("I am already running, likely as a service. Bailing.")
+		return true
+	}
+
+	// So, not running as a service... yet.
+	buf = runCmd("/usr/bin/id", "-u")
+	if buf == nil {
+		return true
+	}
+	whoami := strings.TrimSpace(buf.String())
+
+	// Look for existing service
+	buf = runCmd("/bin/sh", "-c", fmt.Sprintf("/bin/launchctl print gui/%s | grep com.voilaweb.fusion.CraftyThings || true", whoami))
+	if buf == nil {
+		return true
+	}
+	if len(buf.Bytes()) > 0 {
+		return false
+	}
+	log.Info().Msg("Installing myself as a service.")
+
+	// Install executable
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to find location of executable.")
+		return true
+	}
+	buf = runCmd("/usr/bin/install", exePath, "/usr/local/bin/craftythingshelper")
+	if buf == nil {
+		return true
+	}
+
+	err = ioutil.WriteFile("/tmp/com.voilaweb.fusion.craftythings.plist", []byte(plist), 0600)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to create plist.")
+		return true
+	}
+	// Bootstrap using plist
+	buf = runCmd("/bin/launchctl", "bootstrap", fmt.Sprintf("gui/%s", whoami), "/tmp/com.voilaweb.fusion.craftythings.plist")
+	if buf == nil {
+		return true
+	}
+	return true
+}
+
 func main() {
 	log = zerolog.New(
 		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC1123Z}).Level(
 		zerolog.InfoLevel).With().Timestamp().Logger()
+
+	if selfInstall() {
+		return
+	}
 
 	svcConfig := &service.Config{
 		Name:        "CraftyThings",
